a8a758aee760e7da164da1aacf371a0c1a983d1f
MOPHEADART <adamrhinehart@proton.me>
2024-12-28 17:11:06 -0500

    Added Framework for block implementation

--- src/main/java/net/mophead/moppiesmushrooms/block/ModBlocks.java
+++ src/main/java/net/mophead/moppiesmushrooms/block/ModBlocks.java
@@ -22,7 +22,7 @@ public class ModBlocks {
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.of(MoppiesMushrooms.MOD_ID), block);
+        return Registry.register(Registries.BLOCK, Identifier.of(MoppiesMushrooms.MOD_ID, name), block);
     }
 
     private static void registerBlockItem(String name, Block block) {
