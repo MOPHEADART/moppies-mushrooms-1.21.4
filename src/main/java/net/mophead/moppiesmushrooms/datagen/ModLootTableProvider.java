7c8f484e1599de9e84776fccff4580f1b63b0919
MOPHEADART <adamrhinehart@proton.me>
2024-12-30 08:27:27 -0500

    Implemented Datagen

--- src/main/java/net/mophead/moppiesmushrooms/datagen/ModLootTableProvider.java
+++ src/main/java/net/mophead/moppiesmushrooms/datagen/ModLootTableProvider.java
@@ -2,17 +2,17 @@ package net.mophead.moppiesmushrooms.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
-import net.minecraft.block.Block;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.Item;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.entry.LeafEntry;
-import net.minecraft.loot.function.ApplyBonusLootFunction;
-import net.minecraft.loot.function.SetCountLootFunction;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
-import net.minecraft.registry.RegistryKeys;
+//import net.minecraft.block.Block;
+//import net.minecraft.enchantment.Enchantment;
+//import net.minecraft.enchantment.Enchantments;
+//import net.minecraft.item.Item;
+//import net.minecraft.loot.LootTable;
+//import net.minecraft.loot.entry.ItemEntry;
+//import net.minecraft.loot.entry.LeafEntry;
+//import net.minecraft.loot.function.ApplyBonusLootFunction;
+//import net.minecraft.loot.function.SetCountLootFunction;
+//import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+//import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.mophead.moppiesmushrooms.block.ModBlocks;
 
--- src/main/java/net/mophead/moppiesmushrooms/datagen/ModModelProvider.java
+++ src/main/java/net/mophead/moppiesmushrooms/datagen/ModModelProvider.java
@@ -4,7 +4,6 @@ import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.BlockStateModelGenerator;
 import net.minecraft.client.data.ItemModelGenerator;
-import net.minecraft.client.data.Model;
 import net.minecraft.client.data.Models;
 import net.mophead.moppiesmushrooms.block.ModBlocks;
 import net.mophead.moppiesmushrooms.item.ModItems;
